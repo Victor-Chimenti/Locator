0bb53a0578a6c1083f12a702000070806c05898f
Victor Chimenti <chimenti@seattleu.edu>
2020-02-22 10:56:09 -0800

    db works

--- Startup.cs
+++ Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Locator.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +25,8 @@ namespace Locator
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connection = Configuration.GetConnectionString("ATMLocatorDatabase");
+            services.AddDbContext<MaphawksContext>(options => options.UseSqlServer(connection, providerOptions => providerOptions.EnableRetryOnFailure()));
             services.AddControllersWithViews();
         }
 
