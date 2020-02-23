cee58a046a96cf680d3383324b7c19c9c10b2088
Vic Chimenti <chimenti@seattleu.edu>
2020-02-22 20:15:20 -0800

    looking at view model and page model

--- Models/LocationCard.cs
+++ Models/LocationCard.cs
@@ -8,81 +8,10 @@ namespace Locator.Models
 {
     public class LocationCard {
 
-        //public virtual Contacts Contacts { get; set; }
-        //public virtual DailyHours DailyHours { get; set; }
-        //public virtual SpecialQualities SpecialQualities { get; set; }
+        public int CardId { get; set; }
+        public string CardName { get; set; }
 
-
-        ///**
-        // * Card Contact Info
-        // */
-        //[DisplayName("Location ID")]
-        //public string LocationId { get; set; }
-        //[DisplayName("Name")]
-        //public string Name { get; set; }
-        //[DisplayName("Phone")]
-        //public string Phone { get; set; }
-        //[DisplayName("Address")]
-        //public string Address { get; set; }
-        //[DisplayName("City")]
-        //public string City { get; set; }
-        //[DisplayName("State")]
-        //public string State { get; set; }
-        //[DisplayName("Zip Code")]
-        //public string PostalCode { get; set; }
-        //[DisplayName("Web Address")]
-        //public string WebAddress { get; set; }
-        //[DisplayName("Location Type")]
-        //public string LocationType { get; set; }
-
-
-        ///**
-        // * Card Sub Title
-        // */
-        //[DisplayName("Hours")]
-        //public string Hours { get; set; }
-        //[DisplayName("Retail Outlet")]
-        //public string RetailOutlet { get; set; }
-        //[DisplayName("Installation Type")]
-        //public string InstallationType { get; set; }
-        //[DisplayName("Notes")]
-        //public string AccessNotes { get; set; }
-
-
-        ///**
-        // * Card List Items
-        // */
-        //[DisplayName("Handicap Access")]
-        //public string HandicapAccess { get; set; }
-        //[DisplayName("Surcharge")]
-        //public string Surcharge { get; set; }
-        //[DisplayName("Drive Thru Only")]
-        //public string DriveThruOnly { get; set; }
-        //[DisplayName("Accepts Deposit")]
-        //public string AcceptDeposit { get; set; }
-        //[DisplayName("Accepts Cash")]
-        //public string AcceptCash { get; set; }
-        //[DisplayName("Cashless")]
-        //public string Cashless { get; set; }
-        //[DisplayName("Self Service Device")]
-        //public string SelfServiceDevice { get; set; }
-        //[DisplayName("Self Service Only")]
-        //public string SelfServiceOnly { get; set; }
-        //[DisplayName("On Military Base")]
-        //public string OnMilitaryBase { get; set; }
-        //[DisplayName("Military ID Required")]
-        //public string MilitaryIdRequired { get; set; }
-        //[DisplayName("Restricted Access")]
-        //public string RestrictedAccess { get; set; }
-
-
-        ///**
-        // * Card Coordinates
-        // */
-        //[DisplayName("Latitude")]
-        //public decimal Latitude { get; set; }
-        //[DisplayName("Longitude")]
-        //public decimal Longitude { get; set; }
+        public virtual ICollection<Locations> Location { get; set; }
 
     };
 }
---
+++ ViewModels/LocationViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Locator.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Locator.ViewModels
+{
+    public class LocationViewModel
+    {
+        public Guid Id { get; set; }
+
+        //[Required(ErrorMessage = "required")]
+        public string LocationName { get; set; }
+
+        public int SelectedValue { get; set; }
+
+        public virtual LocationCard LocationCard { get; set; }
+
+        [DisplayName("Location Card")]
+        public virtual ICollection<LocationCard> LocationCards { get; set; }
+    }
+}
