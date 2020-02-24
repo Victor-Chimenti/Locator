570301ef53d6cc959b9900365f0b114f778cd99e
Vic Chimenti <chimenti@seattleu.edu>
2020-02-23 21:20:13 -0800

    contacts annotations

--- Models/Contacts.cs
+++ Models/Contacts.cs
@@ -1,14 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Locator.Models
 {
     public partial class Contacts
     {
-        public string LocationId { get; set; }
+        [Phone]
+        [Display(Name = "Phone")]
         public string Phone { get; set; }
-        public string Fax { get; set; }
+        [Url]
+        [Display(Name = "Web Address")]
         public string WebAddress { get; set; }
+
+
+
+        public string Fax { get; set; }
+        public string LocationId { get; set; }
 
         public virtual Locations Location { get; set; }
     }
