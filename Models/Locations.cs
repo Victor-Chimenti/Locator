8fc371c8433598b572421a5d363002e709b594cf
Vic Chimenti <chimenti@seattleu.edu>
2020-02-23 20:14:15 -0800

    add data annotations

--- Models/Locations.cs
+++ Models/Locations.cs
@@ -1,33 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Locator.Models
 {
     public partial class Locations
     {
-
+        [Key]
         public string LocationId { get; set; }
-        public string CoopLocationId { get; set; }
-        public bool TakeCoopData { get; set; }
-        public bool SoftDelete { get; set; }
+        [Display(Name = "Name")]
         public string Name { get; set; }
+        [Display(Name = "Address")]
         public string Address { get; set; }
+        [Display(Name = "City")]
         public string City { get; set; }
-        public string County { get; set; }
+        [Display(Name = "State")]
         public string State { get; set; }
+        [Display(Name = "Zip Code")]
         public string PostalCode { get; set; }
-        public string Country { get; set; }
+        [Display(Name = "Latitude")]
         public decimal Latitude { get; set; }
+        [Display(Name = "Longitude")]
         public decimal Longitude { get; set; }
+        [Display(Name = "Hours")]
         public string Hours { get; set; }
+        [Display(Name = "Retail Outlet")]
         public string RetailOutlet { get; set; }
+        [Display(Name = "Location Type")]
         public string LocationType { get; set; }
 
         public virtual Contacts Contacts { get; set; }
         public virtual DailyHours DailyHours { get; set; }
         public virtual SpecialQualities SpecialQualities { get; set; }
 
+
+
+
+        public string CoopLocationId { get; set; }
+        public bool TakeCoopData { get; set; }
+        public bool SoftDelete { get; set; }
+        public string County { get; set; }
+        public string Country { get; set; }
 
 
 
--- Models/SpecialQualities.cs
+++ Models/SpecialQualities.cs
@@ -1,29 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Locator.Models
 {
     public partial class SpecialQualities
     {
-        public string LocationId { get; set; }
-        public string RestrictedAccess { get; set; }
-        public string AcceptDeposit { get; set; }
-        public string AcceptCash { get; set; }
-        public string EnvelopeRequired { get; set; }
-        public string OnMilitaryBase { get; set; }
-        public string OnPremise { get; set; }
-        public string Surcharge { get; set; }
-        public string Access { get; set; }
-        public string AccessNotes { get; set; }
+        [Display(Name = "Installation Type")]
         public string InstallationType { get; set; }
-        public string HandicapAccess { get; set; }
-        public string Cashless { get; set; }
-        public string DriveThruOnly { get; set; }
-        public string LimitedTransactions { get; set; }
-        public string MilitaryIdRequired { get; set; }
-        public string SelfServiceDevice { get; set; }
-        public string SelfServiceOnly { get; set; }
+        [Display(Name = "Notes")]
+        public string AccessNotes { get; set; }
 
+        [Display(Name = "Handicap Access")]
+        public string HandicapAccess { get; set; }
+        [Display(Name = "Drive Thru Only")]
+        public string DriveThruOnly { get; set; }
+        [Display(Name = "Surcharge")]
+        public string Surcharge { get; set; }
+        [Display(Name = "Accept Deposit")]
+        public string AcceptDeposit { get; set; }
+        [Display(Name = "Accept Cash")]
+        public string AcceptCash { get; set; }
+        [Display(Name = "Cashless")]
+        public string Cashless { get; set; }
+        [Display(Name = "Self Service Device")]
+        public string SelfServiceDevice { get; set; }
+        [Display(Name = "Self Service Only")]
+        public string SelfServiceOnly { get; set; }
+        [Display(Name = "On Military Base")]
+        public string OnMilitaryBase { get; set; }
+        [Display(Name = "Military ID Required")]
+        public string MilitaryIdRequired { get; set; }
+        [Display(Name = "Restricted Access")]
+        public string RestrictedAccess { get; set; }
+
+
+
+
+        public string LimitedTransactions { get; set; }
+        public string EnvelopeRequired { get; set; }
+        public string OnPremise { get; set; }
+        public string Access { get; set; }
+
+        public string LocationId { get; set; }
         public virtual Locations Location { get; set; }
     }
 }
