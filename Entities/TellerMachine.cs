c6fa063d9fdd30ae249387e8d92ded56de3f7838
Vic Chimenti <chimenti@seattleu.edu>
2020-02-24 21:36:28 -0800

    inheritance

--- Entities/TellerMachine.cs
+++ Entities/TellerMachine.cs
@@ -11,34 +11,9 @@ using Locator.Models;
 
 namespace Locator.Entities
 {
-    public class TellerMachine
+    // Child class inherits from the Locations Model
+    public class TellerMachine : Locations
     {
-
-        [Key]
-        public string LocationId { get; set; }
-        [Display(Name = "Name")]
-        public string Name { get; set; }
-        [Display(Name = "Address")]
-        public string Address { get; set; }
-        [Display(Name = "City")]
-        public string City { get; set; }
-        [Display(Name = "State")]
-        public string State { get; set; }
-        [Display(Name = "Zip Code")]
-        public string PostalCode { get; set; }
-        [Display(Name = "Latitude")]
-        public decimal Latitude { get; set; }
-        [Display(Name = "Longitude")]
-        public decimal Longitude { get; set; }
-        [Display(Name = "Hours")]
-        public string Hours { get; set; }
-        [Display(Name = "Retail Outlet")]
-        public string RetailOutlet { get; set; }
-        [Display(Name = "Location Type")]
-        public string LocationType { get; set; }
-        public virtual Contacts Contacts { get; set; }
-        public virtual DailyHours DailyHours { get; set; }
-        public virtual SpecialQualities SpecialQualities { get; set; }
 
         [Column(TypeName = "geometry")]
         public IPoint Location { get; set; }
