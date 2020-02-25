70851bc3226da84e0b961f9a642871500da8205e
Vic Chimenti <chimenti@seattleu.edu>
2020-02-24 23:09:35 -0800

    index view model

---
+++ ViewModels/IndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Locator.ViewModels
+{
+    public class IndexViewModel
+    {
+        public LocationInputModel LocationInput { get; set; }
+        public List<TellerMachineViewModel> TellerMachines { get; set; }
+    }
+}
