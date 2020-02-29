9665955af04da52bec0617fe6ab46bb7be950753
Vic Chimenti <chimenti@seattleu.edu>
2020-02-29 15:52:59 -0800

    data anno point table

--- Models/PointTable.cs
+++ Models/PointTable.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using NetTopologySuite.Geometries;
 
 namespace Locator.Models
 {
     public partial class PointTable
     {
-        public string LocationId { get; set; }
+        [Column(TypeName = "geometry")]
         public Geometry Point { get; set; }
 
+
         public virtual Locations Location { get; set; }
+        public string LocationId { get; set; }
+
     }
 }
