1256a7a576ef65b6a0daef70e9eff1c6d8a8001b
Vic Chimenti <chimenti@seattleu.edu>
2020-03-02 11:27:41 -0800

    ready for pub attempt

--- Locator/Controllers/LocationsController.cs
+++ Locator/Controllers/LocationsController.cs
@@ -53,7 +53,7 @@ namespace Locator.Controllers
             }
 
             // TODO, for now filter down to just 3 records
-            data = data.GetRange(0, 89).ToList();
+            data = data.GetRange(0, 10).ToList();
 
             return View(data);
         }
