883748154303e2470c992715879aecf4dd3dfa44
Vic Chimenti <chimenti@seattleu.edu>
2020-03-03 14:55:07 -0800

    database helper declarations

---
+++ Locator/Backend/IDatabaseHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+using DatabaseLibrary.Models;
+using System.Collections.Generic;
+using Locator.Models;
+
+namespace Locator.Backend
+{
+    public class IDatabaseHelper
+    {
+
+        public Task<List<Locations>> ReadMultipleRecordsAsync(bool isDeleted = false);
+
+        public bool LocationIdNotUnique(string id);
+    }
+}
