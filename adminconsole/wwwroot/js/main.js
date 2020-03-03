b158ae25e4f5cb8b0e856e17019f6b1829658ca7
Jake Ladera <laderah@seattleu.edu>
2020-03-03 11:13:55 -0800

    Changed == to ===

--- adminconsole/wwwroot/js/main.js
+++ adminconsole/wwwroot/js/main.js
@@ -11,7 +11,7 @@
         var check = true;
 
         for(var i=0; i<input.length; i++) {
-            if(validate(input[i]) == false){
+            if(validate(input[i]) === false){
                 showValidate(input[i]);
                 check=false;
             }
@@ -28,13 +28,13 @@
     });
 
     function validate (input) {
-        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+        if($(input).attr('type') === 'email' || $(input).attr('name') === 'email') {
+            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) === null) {
                 return false;
             }
         }
         else {
-            if($(input).val().trim() == ''){
+            if($(input).val().trim() === ''){
                 return false;
             }
         }
@@ -56,7 +56,7 @@
     [ Show pass ]*/
     var showPass = 0;
     $('.btn-show-pass').on('click', function(){
-        if(showPass == 0) {
+        if(showPass === 0) {
             $(this).next('input').attr('type','text');
             $(this).find('i').removeClass('fa-eye');
             $(this).find('i').addClass('fa-eye-slash');
