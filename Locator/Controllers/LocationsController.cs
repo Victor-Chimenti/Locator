30fe548fb94d14e336149b78797d41afc53de0f9
Victor Chimenti <chimenti@seattleu.edu>
2020-03-04 07:11:45 -0800

    ready to dev

--- Locator/Controllers/LocationsController.cs
+++ Locator/Controllers/LocationsController.cs
@@ -73,7 +73,7 @@ namespace Locator.Controllers
             var cleanResults = new CleanLocationViewModel(dirtyResults);
 
             //// TODO, for now filter down to just num records
-            cleanResults.CleanLocationList = cleanResults.CleanLocationList.GetRange(0, 28);
+            cleanResults.CleanLocationList = cleanResults.CleanLocationList.GetRange(0, 289);
 
             return cleanResults;
         }
