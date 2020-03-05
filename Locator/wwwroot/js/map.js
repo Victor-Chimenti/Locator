86afd67ebe94b33d6a51107db6c7bbf43416c380
Vic Chimenti <chimenti@seattleu.edu>
2020-03-05 09:29:28 -0800

    add basic map options

--- Locator/wwwroot/js/map.js
+++ Locator/wwwroot/js/map.js
@@ -4,3 +4,37 @@
  */
 
 
+
+
+
+
+
+// ***  display the Google map with the default Options  *** //
+infoWindow = new google.maps.InfoWindow();
+directionsService = new google.maps.DirectionsService();
+directionsRenderer = new google.maps.DirectionsRenderer();
+
+// *** initialize directions renderer *** //
+directionsRenderer.setMap(map);
+directionsRenderer.setPanel(document.getElementById('directionsPanel'));
+
+function initMap() {
+
+    // set BECU Headquarters for default map starting location
+    var headquarters = {
+        lat: 47.490209,
+        lng: -122.272126
+    };
+
+    // *** set default map parameters *** //  
+    var mapOptions = {
+        center: headquarters,
+        gestureHandling: 'cooperative',
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        //streetViewControl: false,
+        zoom: 15,
+    };
+
+    // *** render map to index page map-wrapper div in the center-index *** //
+    map = new google.maps.Map(document.getElementById('map-wrapper'), mapOptions); 
+}
