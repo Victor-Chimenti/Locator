56612323207b23cc84849f10f6a8c586de2643ed
Vic Chimenti <chimenti@seattleu.edu>
2020-03-07 10:02:15 -0800

    update null check on special qualities

--- Locator/Controllers/LocationsController.cs
+++ Locator/Controllers/LocationsController.cs
@@ -73,7 +73,7 @@ namespace Locator.Controllers
 
 
             var cleanResults = new CleanLocationViewModel(dirtyResults);
-            cleanResults.CleanLocationList = cleanResults.CleanLocationList.GetRange(0, 12);
+            cleanResults.CleanLocationList = cleanResults.CleanLocationList.GetRange(0, 612);
 
             return cleanResults;
         }
