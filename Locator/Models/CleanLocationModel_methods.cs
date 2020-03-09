ebccbdac0d07bb5ce0cc1423462a731bc4075c47
Vic Chimenti <chimenti@seattleu.edu>
2020-03-09 10:54:09 -0700

    ud

--- Locator/Models/CleanLocationModel_methods.cs
+++ Locator/Models/CleanLocationModel_methods.cs
@@ -80,19 +80,6 @@ namespace Locator.Models
 
                 var defaultLatStr = "47.490209";
                 var defaultLngStr = "-122.272126";
-
- /*               var csWgs84 = ProjNet.CoordinateSystems.GeographicCoordinateSystems.WGS84;
-                const string epsg27700 = "..."; // see http://epsg.io/27700
-                var cs27700 = ProjNet.Converters.WellKnownText.CoordinateSystemWktReader.Parse(epsg27700);
-                var ctFactory = new ProjNet.CoordinateSystems.Transformations.CoordinateTransformationFactory();
-                var ct = ctFactory.CreateFromCoordinateSystems(csWgs84, cs27700);
-                var mt = ct.MathTransform;*/
-
-                //if ((!Latitude.Equals(null)) && (!Longitude.Equals(null)))
-                //{
-                //    defaultLat = Decimal.ToDouble(Latitude);
-                //    defaultLng = Decimal.ToDouble(Longitude);
-                //}
                 var defaultPosition = new PositionModel(defaultLatStr, defaultLngStr);
                 var geometryFactory = NtsGeometryServices.Instance.CreateGeometryFactory(srid: 4326);
                 MyPoint = geometryFactory.CreatePoint(new NetTopologySuite.Geometries.Coordinate(Position.Lat, Position.Lng));
